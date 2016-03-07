11584571c144242d4dfdb955c7dc9de1742e30dc
PeridexisErrant <PeridexisErrant@gmail.com>
2016-03-08 00:11:38 +1100

    Moved .tex files to subdir

--- _make.cmd
+++ _make.cmd
@@ -1,3 +1,3 @@
 @echo off
 :: Do a clean build, and exit on success - but show log on error
-texify.exe --clean --batch --pdf --quiet --job-name="_Zac_Thesis.pdf" 0_outline.tex || pause
+texify.exe --clean --batch --pdf --quiet --job-name="_Zac_Thesis.pdf" "tex\0_outline.tex" || pause
