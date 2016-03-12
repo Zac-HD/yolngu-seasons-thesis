d9e5b41a3e7a22c38dea83d1a451e30a53d2525d
PeridexisErrant <PeridexisErrant@gmail.com>
2016-03-12 12:33:06 +1100

    Split style, outline; abstract styling

--- _make.cmd
+++ _make.cmd
@@ -1,3 +1,3 @@
 @echo off
 :: Do a clean build, and exit on success - but show log on error
-texify.exe --clean --batch --pdf --quiet --job-name="_Zac_Thesis" "tex\0_outline.tex" || pause
+texify.exe --clean --batch --pdf --quiet --job-name="_Zac_Thesis" "tex\thesis.tex" || pause
