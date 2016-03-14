d5a81ed1dcf21b287a67c2f32d4b7eb4ac4ed71c
PeridexisErrant <PeridexisErrant@gmail.com>
2016-03-14 19:42:11 +1100

    Edit filenames

--- _make.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Do a clean build, and exit on success - but show log on error
-texify.exe --clean --batch --pdf --quiet --job-name="_Zac_Thesis" "tex\thesis.tex" || pause
---
+++ make.cmd
@@ -0,0 +1,3 @@
+@echo off
+:: Do a clean build, and exit on success - but show log on error
+texify.exe --clean --batch --pdf --quiet --job-name="Zac_Thesis" "tex\thesis.tex" || pause
