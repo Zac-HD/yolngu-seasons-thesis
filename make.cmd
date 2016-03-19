8a8e822372c2eb090b8babafcd880c7d78cda39c
Zac-HD <zac.hatfield.dodds@gmail.com>
2016-03-19 15:27:55 +1100

    Use 12pt, open pdf after build

--- make.cmd
+++ make.cmd
@@ -1,3 +1,3 @@
 @echo off
 :: Do a clean build, and exit on success - but show log on error
-texify.exe --clean --batch --pdf --quiet --job-name="Zac_Thesis" "tex\thesis.tex" || pause
+texify.exe --clean --batch --pdf --quiet --job-name="Zac_Thesis" "tex\thesis.tex" --run-viewer || pause
